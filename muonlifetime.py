e74f874710ebf9887a09e459f54274edb830007e
tomkooij <tomkooij@tomkooij.nl>
2015-03-13 11:39:50 +0100

    fit exponential withou linear term

--- muonlifetime.py
+++ muonlifetime.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
-fit_func  = lambda x,a,b,c : a*np.exp(b*x)+c
+fit_func  = lambda x,a,b : a*np.exp(b*x)
 
 if __name__=='__main__':
     # open .txt
@@ -18,7 +18,7 @@ if __name__=='__main__':
     n, bins, bla = plt.hist(lifetime, bins=np.arange(20.,1500.,20.), histtype='step')
 
     # Fit een dalende exponent
-    initialguess = [100., -0.05, 0.]
+    initialguess = [100., -0.05]
     middle = [(bins[i]+bins[i+1])/2 for i in range(len(bins)-1)]
     c, cov = curve_fit(fit_func, middle, n, p0=initialguess)
     print "fit result: ",c
@@ -29,7 +29,7 @@ if __name__=='__main__':
     print "\nError (measured/real liftime ) = %2.2f" % float(2196./muonlifetime)
     # plot de gefitte functie ook in de grafiek
     xfit = np.array(middle)
-    yfit = fit_func(xfit, c[0], c[1], c[2])
+    yfit = fit_func(xfit, c[0], c[1])
     plt.plot(xfit, yfit, 'r--')
     plt.legend(['fit lifetime = %4.f ns' % muonlifetime, 'data'])
     plt.show()
